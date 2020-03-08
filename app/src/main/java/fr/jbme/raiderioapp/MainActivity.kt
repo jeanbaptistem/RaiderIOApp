0bbe25d9946e3b5111f0795846e3b643b379d157
jeanbaptistem <jb.mesnil98@gmail.com>
2020-03-08 15:44:38 +0100

    Add character thumbnail using Picasso

--- app/src/main/java/fr/jbme/raiderioapp/MainActivity.kt
+++ app/src/main/java/fr/jbme/raiderioapp/MainActivity.kt
@@ -22,19 +22,28 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
 import fr.jbme.raiderioapp.data.contants.CHARACTER_NAME_KEY
 import fr.jbme.raiderioapp.data.contants.REALM_NAME_KEY
 import fr.jbme.raiderioapp.data.contants.REGION_KEY
 import fr.jbme.raiderioapp.data.contants.SHARED_PREF_KEY
+import fr.jbme.raiderioapp.data.model.character.CharacterResponse
+import fr.jbme.raiderioapp.network.RaiderIOService
+import fr.jbme.raiderioapp.network.RetrofitInstance
+import fr.jbme.raiderioapp.network.utils.NetworkErrorUtils
 import fr.jbme.raiderioapp.ui.login.LoginViewModel
 import fr.jbme.raiderioapp.ui.login.LoginViewModelFactory
 import kotlinx.android.synthetic.main.app_bar_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sharedPref: SharedPreferences
+    private var raiderIOService: RaiderIOService? = null
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var loginViewModel: LoginViewModel
@@ -52,6 +61,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         sharedPref = getSharedPreferences(SHARED_PREF_KEY, Context.MODE_PRIVATE)
+        raiderIOService = RetrofitInstance.retrofitInstance?.create(RaiderIOService::class.java)
         loginViewModel =
             ViewModelProviders.of(this, LoginViewModelFactory()).get(LoginViewModel::class.java)
 
@@ -116,22 +126,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initNavHeader() {
-        with(sharedPref) {
-            navHeaderTitle.text = getString(
-                CHARACTER_NAME_KEY,
-                applicationContext.getString(R.string.nav_header_title)
-            )
-            navHeaderDescription.text =
-                String.format(
-                    "%s %s",
-                    getString(
-                        REALM_NAME_KEY,
-                        applicationContext.getString(R.string.nav_header_desc)
-                    ),
-                    getString(
-                        REGION_KEY, ""
-                    )?.toUpperCase(Locale.ROOT)
-                )
-        }
+        val characterName: String =
+            sharedPref.getString(CHARACTER_NAME_KEY, getString(R.string.nav_header_title))!!
+        val realmName: String =
+            sharedPref.getString(REALM_NAME_KEY, getString(R.string.nav_header_desc))!!
+        val region: String = sharedPref.getString(REGION_KEY, "")?.toUpperCase(Locale.ROOT)!!
+
+        val call = raiderIOService?.getCharacterInfo(region, realmName, characterName)
+        call?.enqueue(object : Callback<CharacterResponse> {
+            override fun onFailure(call: Call<CharacterResponse>, t: Throwable) {
+                loginViewModel.logout()
+                Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                applicationContext.startActivity(intent)
+            }
+
+            override fun onResponse(
+                call: Call<CharacterResponse>,
+                response: Response<CharacterResponse>
+            ) {
+                if (response.isSuccessful) {
+                    val character = response.body()!!
+                    navHeaderTitle.text = character.name
+                    navHeaderDescription.text = String.format(
+                        "%s %s", character.realm, character.region.toUpperCase(
+                            Locale.ROOT
+                        )
+                    )
+                    Picasso.get().load(character.thumbnailUrl).resize(180, 180)
+                        .into(navHeaderThumbnail)
+                } else {
+                    val errorResponse =
+                        NetworkErrorUtils.parseError(response)
+                    onFailure(call, Exception(errorResponse.message))
+                }
+            }
+
+        })
     }
 }
